d5dc0ac9ede042e0ff6da80327baef118b385965
Jared Williams <jareddw@berkeley.edu>
2022-07-24 12:11:39 -0700

    added .env file for yelp api key.

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {YELP_API_KEY} from '@env';
 
 export default function App() {
+
+  let api_key = YELP_API_KEY;
+
   return (
     <View style={styles.container}>
       <Text>Hiiiii there</Text>
-      <StatusBar style="auto" /> 
+      <StatusBar style="auto" />
     </View>
   );
 }
@@ -13,7 +17,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'lightblue',
     alignItems: 'center',
     justifyContent: 'center',
   },
